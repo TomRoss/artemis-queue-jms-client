a279af99713a4802d93b32a49d8b31475b560d9a
Tom Ross <toross@redhat.com>
2022-02-22 09:02:02 +0000

    Added consumer and prodcuer shell scripts

---
+++ src/main/resources/artemis-consumer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+java -jar -Dconnection.name=ConnectionFactory \
+-Dqueue.name=jms/queue/testQueue \
+-Dclient.type=consumer \
+-Dreceive.timeout=30000 \
+-Dlog.batch.size=1 \
+-Djavax.net.ssl.trustStore=/home/tomr/Work/OpenShift/amq-broker/ssl-cert/client-ts.jks \
+-Djavax.net.ssl.trustStorePassword=secret \
+-Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+-Djava.util.logging.config.file=/github/repos/cee/artemis-queue-jms-client/src/main/resources/logging.properties \
+artemis-jms-queue-client.jar
